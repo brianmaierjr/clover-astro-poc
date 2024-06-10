58a652b16cc1bfee67c1e3688b8b17340f488345
Brian Maier <brimaidesigns@gmail.com>
2024-06-10 15:27:55 -0400

    + image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	image: {
+		domains: ["d36ai2hkxl16us.cloudfront.net"],
+	},
+});
