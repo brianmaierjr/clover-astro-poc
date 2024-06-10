249e9cedd50369596a0f0ad97cca14a655d458b9
Brian Maier <brimaidesigns@gmail.com>
2024-06-10 19:19:56 -0400

    + about page and images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
 	image: {
 		domains: ["d36ai2hkxl16us.cloudfront.net"],
 	},
+	integrations: [
+		sanity({
+			projectId: "w5qbj6n9",
+			dataset: "production",
+			// Set useCdn to false if you're building statically.
+			useCdn: false,
+		}),
+	],
 });
