cfd57a5b3758a5b6f527113a12c7140442658591
Brian Maier <brimaidesigns@gmail.com>
2024-06-25 15:39:46 -0400

    + sass for main stylesheet and components for base head and navigation

---
+++ src/data/settings.ts
@@ -0,0 +1,14 @@
+// Main Site Settings
+export const settings = {
+	site: "https://cloverlearning.com",
+	title: "Clover Learning | Diagnostic Imaging Training, Exam Prep, and Continuing Education",
+	description:
+		"Supporting a new generation of students & professionals through video-based Radiography training & Prep for the ARRT® Registry Exam",
+};
+
+// Main Menu
+export const menu = [
+	{ name: "Home", link: "/" },
+	{ name: "Components", link: "/components" },
+	{ name: "Contact", link: "/contact" },
+];
