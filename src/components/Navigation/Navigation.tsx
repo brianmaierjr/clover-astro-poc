134c96ab49d6cb40e9f8ccd1687179d73869ce17
Brian Maier <brimaidesigns@gmail.com>
2024-07-03 17:00:27 -0400

    update tokens, main styling setup

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,17 @@
+import "./Navigation.css";
+
+export default function Navigation() {
+	return (
+		<ul>
+			<li>
+				<a href='/'>Home</a>
+			</li>
+			<li>
+				<a href='/about'>About</a>
+			</li>
+			<li>
+				<a href='/typography'>Typography</a>
+			</li>
+		</ul>
+	);
+}
